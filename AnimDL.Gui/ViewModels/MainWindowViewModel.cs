8e1ae8aeb3a03eda2d2f75d37274101e5a5196b7
Athul Raj <athulrajts@gmail.com>
2022-07-17 19:46:48 +0530

    add gui project

---
+++ AnimDL.Gui/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AnimDL.Gui.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to Avalonia!";
+    }
+}
